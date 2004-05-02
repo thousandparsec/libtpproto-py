2088f55b6c2fa9ccbb41194c5c786a0be3977d4a
Tim Ansell <tim@thousandparsec.net>
2004-05-02 17:31:52 +0000

    Should no longer cause epydoc to hang.

--- test.py
+++ test.py
@@ -11,52 +11,54 @@ from tests.python.Object_tests import tests as Object_tests
 from tests.python.Order_Get_tests import tests as Order_Get_tests
 from tests.python.Order_Remove_tests import tests as Order_Remove_tests
 
-print "OK"
-for t in OK_tests:
-	t, error = t()
-	print "	", error
-	
-print "Fail"
-for t in Fail_tests:
-	t, error = t()
-	print "	", error
-	
-print "Sequence"
-for t in Sequence_tests:
-	t, error = t()
-	print "	", error
+if __name__ == "__main__":
 
-print "Connect"
-for t in Connect_tests:
-	t, error = t()
-	print "	", error
+	print "OK"
+	for t in OK_tests:
+		t, error = t()
+		print "	", error
+		
+	print "Fail"
+	for t in Fail_tests:
+		t, error = t()
+		print "	", error
+		
+	print "Sequence"
+	for t in Sequence_tests:
+		t, error = t()
+		print "	", error
 
-print "Login"
-for t in Login_tests:
-	t, error = t()
-	print "	", error
+	print "Connect"
+	for t in Connect_tests:
+		t, error = t()
+		print "	", error
 
-print "Object_GetById"
-for t in Object_GetById_tests:
-	t, error = t()
-	print "	", error
+	print "Login"
+	for t in Login_tests:
+		t, error = t()
+		print "	", error
 
-print "Object_GetByPos"
-for t in Object_GetByPos_tests:
-	t, error = t()
-	print "	", error
+	print "Object_GetById"
+	for t in Object_GetById_tests:
+		t, error = t()
+		print "	", error
 
-print "Object"
-for t in Object_tests:
-	t, error = t()
-	print "	", error
-	
-print "Order_Get"
-for t in Order_Get_tests:
-	t, error = t()
-	print "	", error
+	print "Object_GetByPos"
+	for t in Object_GetByPos_tests:
+		t, error = t()
+		print "	", error
 
-print "Order_Remove"
-for t in Order_Remove_tests:
-	t, error = t()
-	print "	", error
+	print "Object"
+	for t in Object_tests:
+		t, error = t()
+		print "	", error
+		
+	print "Order_Get"
+	for t in Order_Get_tests:
+		t, error = t()
+		print "	", error
+
+	print "Order_Remove"
+	for t in Order_Remove_tests:
+		t, error = t()
+		print "	", error
