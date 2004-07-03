6efdf2e952dc97fb8b85cf49e6d54ac77e64f078
Tim Ansell <tim@thousandparsec.net>
2004-07-03 21:00:52 +0000

    Small updates..

--- support/output.py
+++ support/output.py
@@ -6,11 +6,17 @@ def red(string, flush=1):
 	sys.stdout.write(string)
 	sys.stdout.write("[0m")
 	if flush:
-		sys.stdout.flush()
+		try:
+			sys.stdout.flush()
+		except:
+			pass
 
 def green(string, flush=1):
 	sys.stdout.write("[01;31m")
 	sys.stdout.write(string)
 	sys.stdout.write("[0m")
 	if flush:
-		sys.stdout.flush()
+		try:
+			sys.stdout.flush()
+		except:
+			pass
