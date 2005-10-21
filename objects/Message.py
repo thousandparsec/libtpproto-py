7195ae685158723b2fba7588b477ad7d58f4d27d
Lee Begg <lee@thousandparsec.net>
2005-10-21 00:51:45 +0000

    Fixed Message format, follows spec.

--- objects/Message.py
+++ objects/Message.py
@@ -11,11 +11,13 @@ class Message(Processed):
 		* a list of UInt32, Type of message
 		* a string, Subject of message
 		* a string, Body of the message
+                * UInt32, turn number the messages was generated on
+                * a list of Int32, UInt32 references
 	"""
 	no = 19
-	struct = "Ij[I]SS"
+	struct = "Ij[I]SSI[II]"
 
-	def __init__(self, sequence, id, slot, types, subject, body):
+	def __init__(self, sequence, id, slot, types, subject, body, turn, references):
 		Processed.__init__(self, sequence)
 
 		# Length is:
@@ -23,16 +25,19 @@ class Message(Processed):
 		self.length = 4 + 4 + \
 				4 + len(types)*4 + \
 				4 + len(subject) + \
-				4 + len(body) 
+				4 + len(body) + \
+                                4 + len(references)
 
 		self.id = id
 		self.slot = slot
 		self.types = types
 		self.subject = subject
 		self.body = body
+                self.turn = turn
+                self.references = references
 	
 	def __repr__(self):
 		output = Processed.__repr__(self)
-		output += pack(self.struct, self.id, self.slot, self.types, self.subject, self.body)
+		output += pack(self.struct, self.id, self.slot, self.types, self.subject, self.body, self.turn, self.references)
 
 		return output
