3950a19743512ec488e1e574656f36950e7f833c
Tim Ansell <tim@thousandparsec.net>
2005-12-29 20:56:38 +0000

    Only output color on Linux.

--- support/output.py
+++ support/output.py
@@ -2,9 +2,11 @@
 import sys
 
 def red(string, flush=1):
-	sys.stdout.write("[01;32m")
+	if sys.platform.startswith('linux'):
+		sys.stdout.write("[01;32m")
 	sys.stdout.write(string)
-	sys.stdout.write("[0m")
+	if sys.platform.startswith('linux'):
+		sys.stdout.write("[0m")
 	if flush:
 		try:
 			sys.stdout.flush()
@@ -12,9 +14,11 @@ def red(string, flush=1):
 			pass
 
 def green(string, flush=1):
-	sys.stdout.write("[01;31m")
+	if sys.platform.startswith('linux'):
+		sys.stdout.write("[01;31m")
 	sys.stdout.write(string)
-	sys.stdout.write("[0m")
+	if sys.platform.startswith('linux'):
+		sys.stdout.write("[0m")
 	if flush:
 		try:
 			sys.stdout.flush()
