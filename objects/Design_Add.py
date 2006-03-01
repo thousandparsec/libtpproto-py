66432239e998c311db07e2a8fc52ec5f1b7c2a6d
Tim Ansell <tim@thousandparsec.net>
2006-03-01 18:31:27 +0000

    Design Add doesn't send unneeded fields.

--- objects/Design_Add.py
+++ objects/Design_Add.py
@@ -9,3 +9,11 @@ class Design_Add(Design):
 			*args, **kw):
 		self.no = 49
 		apply(Design.__init__, (self, sequence, id,)+args, kw)
+	
+	def __str__(self):
+		output1 = pack(self.struct, self.id, -1, self.categories, self.name, self.desc, -1, self.owner, self.components, "", [])
+		
+		self.length = len(output1)
+		output2 = Processed.__str__(self)
+
+		return output2+output1
