eb5462b6b782d31eba5d1f33cb004b21dee86748
Tim Ansell <tim@thousandparsec.net>
2006-12-26 09:32:23 +0000

    Start of support for ZeroConf.

---
+++ tp/netlib/zeroconf/avahi_browser.py
@@ -0,0 +1,181 @@
+import os, sys
+
+try:
+	import avahi, avahi.ServiceTypeDatabase
+except ImportError, e:
+	print e
+	sys.exit(1)
+
+import gobject 
+import dbus
+if getattr(dbus, 'version', (0,0,0)) >= (0,41,0):
+    import dbus.glib
+
+service_type_browsers = {}
+service_browsers = {}
+service_type_db = avahi.ServiceTypeDatabase.ServiceTypeDatabase()
+service_seen = {}
+
+class ZeroConfBrowser:
+	######################################
+	# Helper functions
+	######################################
+	def protoname(self,protocol):
+		if protocol == avahi.PROTO_INET:
+			return "IPv4"
+		if protocol == avahi.PROTO_INET6:
+			return "IPv6"
+		return "n/a"
+
+	def siocgifname(self, interface):
+		if interface <= 0:
+			return "n/a"
+		else:
+			return self.server.GetNetworkInterfaceNameByIndex(interface)
+
+	def get_interface_name(self, interface, protocol):
+		if interface == avahi.IF_UNSPEC and protocol == avahi.PROTO_UNSPEC:
+			return "Wide Area"
+		else:
+			return str(self.siocgifname(interface)) + " " + str(self.protoname(protocol))
+
+	def lookup_type(self, stype):
+		global service_type_db
+		try:
+			return service_type_db[stype]
+		except KeyError:
+			return stype
+
+	def print_error(self, err):
+		print "Error:", str(err)
+
+	def pair_to_dict(self, l):
+		res = dict()
+		for el in l:
+			if "=" not in el:
+				res[el]=''
+			else:
+				tmp = el.split('=',1)
+				if len(tmp[0]) > 0:
+					res[tmp[0]] = tmp[1]
+		return res
+
+	######################################
+	# Callback functions
+	######################################
+	def service_resolved(self, interface, protocol, name, stype, domain, host, aprotocol, address, port, txt, flags):
+		"""\
+		Called when all the information about a service is avaliable.
+		"""
+		if len(txt) != 0:
+			details = self.pair_to_dict(avahi.txt_array_to_string_array(txt))
+		else:
+			details = []
+
+		key = "%s-%s-%s" % (name, stype, protocol)
+		global service_seen
+		args = ((host, address), port, stype, name, self.protoname(protocol), details)
+		if not service_seen.has_key(key):
+			self.ServerFound(*args)
+		else:
+			self.ServerRefresh(*args)	
+		service_seen[key] = args
+
+	def new_service(self, interface, protocol, name, stype, domain, flags):
+		"""\
+		Called when a new service is found.
+		"""
+		self.server.ResolveService( interface, protocol, name, stype, domain, \
+			avahi.PROTO_UNSPEC, dbus.UInt32(0), reply_handler=self.service_resolved, error_handler=self.print_error)
+
+	def remove_service(self, interface, protocol, name, stype, domain, flags):
+		"""\
+		Called when a service disappears.
+		"""
+		print interface, protocol, name, stype, domain, flags
+
+		key = "%s-%s-%s" % (name, stype, protocol)
+		global service_seen
+		if service_seen.has_key(key):
+			self.ServerGone(*service_seen[key])
+
+	def new_domain(self, interface, protocol, domain, flags):
+		"""\
+		Called when a new domain appears.
+		"""
+		print self, interface, protocol, domain, flags
+		ifn = self.get_interface_name(interface, protocol)
+		if domain != "local":
+			self.browse_domain(interface, protocol, domain)
+
+	def browse_domain(self, interface, protocol, domain):
+		"""
+		Register to browse a given domain.
+		"""
+		global service_type_browsers
+
+		# Are we already browsing this domain?
+		if service_type_browsers.has_key((interface, protocol, domain)):
+			return
+
+		try:
+			b = dbus.Interface( \
+					self.bus.get_object(avahi.DBUS_NAME, \
+						self.server.ServiceTypeBrowserNew(interface, protocol, domain, dbus.UInt32(0))
+					),  avahi.DBUS_INTERFACE_SERVICE_TYPE_BROWSER)
+		except DBusException, e:
+			print e
+			sys.exit(0)
+
+		for stype in ['_tp', '_tps', '_tphttp', '_tphttps']:
+			stype = stype+'._tcp'
+			b = dbus.Interface( \
+					self.bus.get_object(avahi.DBUS_NAME, \
+						self.server.ServiceBrowserNew(interface, protocol, stype, domain, dbus.UInt32(0))
+					), avahi.DBUS_INTERFACE_SERVICE_BROWSER)
+			b.connect_to_signal('ItemNew', self.new_service)
+			b.connect_to_signal('ItemRemove', self.remove_service)
+			service_browsers[(interface, protocol, stype, domain)] = b
+
+	def __init__(self):
+		self.services_browsed = {}
+
+		self.bus = dbus.SystemBus()
+		self.server = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER), avahi.DBUS_INTERFACE_SERVER)
+
+		# Explicitly browse .local
+		self.browse_domain(avahi.IF_UNSPEC, avahi.PROTO_UNSPEC, "local")
+
+		# Browse for other browsable domains
+		db = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, self.server.DomainBrowserNew(avahi.IF_UNSPEC, avahi.PROTO_UNSPEC, "", avahi.DOMAIN_BROWSER_BROWSE, dbus.UInt32(0))), avahi.DBUS_INTERFACE_DOMAIN_BROWSER)
+		db.connect_to_signal('ItemNew', self.new_domain)
+
+	def run(self):
+		mainloop = gobject.MainLoop()
+		mainloop.run()
+
+	def ServerFound(*args):
+		"""\
+		Called when a new server is found.
+		"""
+		print "ServerFound", args
+
+	def ServerRefresh(*args):
+		"""\
+		Called when we get updated information about a server.
+		"""
+		print "ServerRefresh", args
+
+	def ServerGone(*args):
+		"""\
+		Called when a server goes away.
+		"""
+		print "ServerGone", args
+
+def main():
+	a = ZeroConfBrowser()
+	a.run()
+
+if __name__ == "__main__":
+	main()
+
