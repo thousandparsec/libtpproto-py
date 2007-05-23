157d5c2f680134fa5b9f4f69320259416c46f44b
Tim Ansell <mithro@mithis.com>
2007-05-23 22:56:59 +0930

    Fix the Order Probe message for the new order stuff.

--- tp/netlib/objects/Order_Probe.py
+++ tp/netlib/objects/Order_Probe.py
@@ -4,8 +4,6 @@ from Order import Order
 
 class Order_Probe(Order):
 	no = 34
-	def __init__(self, sequence, \
-			id,	slot, type, \
-			*args, **kw):
+	def __init__(self, *args, **kw):
 		self.no = 34
-		apply(Order.__init__, (self, sequence, id, slot, type, -1, [])+args, kw)
+		Order.__init__(self, *args, **kw)
