939ff4c74aae96c0232be19e2e99628fb43b1a31
Tim Ansell <mithro@mithis.com>
2007-08-06 22:21:57 +0930

    Better error message if xstruct runs out of arguments.

--- tp/netlib/xstruct.py
+++ tp/netlib/xstruct.py
@@ -93,6 +93,9 @@ def pack(sstruct, *aargs):
 			char = struct[0]
 			struct = struct[1:]
 			
+			if len(args) == 0:
+				raise TypeError('Ran out of arguments, still had %s%s left of the structure' % (char, struct))
+
 			if char == ' ' or char == '!':
 				continue
 			elif char == '{':
