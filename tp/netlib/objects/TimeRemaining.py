aff2b4cf79ae082b797c8b8d19d25aff4a23c5f9
Greywhind <noegnud@cox.net>
2009-05-24 17:30:55 -0700

    Updated the TimeRemaining object to the tp04 specifications (added reason, current turn number, and current turn name), to get rid of an error in which its size was larger than expected.

--- tp/netlib/objects/TimeRemaining.py
+++ tp/netlib/objects/TimeRemaining.py
@@ -7,22 +7,39 @@ class TimeRemaining(Processed):
 	"""\
 	The TimeRemaining packet consists of:
 		* UInt32, Seconds left till the turn ends.
+		* 32 bit unsigned enumeration, Reason (why frame was sent):
+		    * 0x0 - This frame was requested.
+		    * 0x1 - Turn timer has been started
+		    * 0x2 - Advanced warning of the turn timer expiring.
+		    * 0x3 - All players have finished and the turn has ended.
+		    * 0x4 - Threshold of players have finished, the timer for the remaining players has started.
+		    * 0x5 - End of turn started.
+		* UInt32, Current turn.
+		* string, Turn name (may be empty).
 	"""
 	no = 15
-	struct = "jI"
+	struct = "jIIS"
 
-	def __init__(self, sequence, time, reason):
+	def __init__(self, sequence, time, reason, turn_num, turn_name):
 		Processed.__init__(self, sequence)
 
 		# Length is:
 		#  * 4 bytes UInt32
-		#
-		self.length = 4
+		#  * 4 bytes UInt32
+		#  * 4 bytes UInt32
+		#  * length of string
+		#  
+		self.length = 4 + \
+						4 + \
+						4 + \
+						4 + len(turn_name)
 		self.time = time
 		self.reason = reason	
+		self.turn_num = turn_num;
+		self.turn_name = turn_name;
 
 	def __str__(self):
 		output = Processed.__str__(self)
-		output += pack(self.struct, self.time, self.reason)
+		output += pack(self.struct, self.time, self.reason, self.turn_num, self.turn_name)
 
 		return output
